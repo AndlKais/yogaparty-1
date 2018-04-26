ef75e50f650805458df57148a8fba35420a4eb7b
Dusan Resavac <resavac.dusan@live.at>
2018-04-26 15:25:42 +0200

    auswahl Style bearbeitet

--- app/components/auswahl.js
+++ app/components/auswahl.js
@@ -9,8 +9,19 @@ app.component("auswahl", {
 
 });
 
-app.controller("auswahlController", function ($log) {
-    this.blockAuswahl = function(arg){
+app.controller("auswahlController", function ($log, $element) {
+
+    this.blockAuswahl = function(arg, $event){
+        let elements = $element[0].childNodes[2].childNodes;
+        elements.forEach(e => {
+            if(e !== $event.currentTarget && e.tagName === "DIV"){
+                if(e.style) {
+                    e.style.boxShadow = "2px 2px 5px 0px rgba(122,122,122,0.55)";
+                }
+            }
+        });
+
+        $event.currentTarget.style.boxShadow = " 0px 0px 20px 4px #2868A6";
         this.ausgewaehlt({
             "ausgewaehlt": arg
         });
