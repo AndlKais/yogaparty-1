108579b56018340e40a38aabf50fa630faee5219
Andreas Kaiser <4171@htl.rennweg.at>
2018-06-10 00:40:38 +0200

    kleine Änderungen in der PHP Datei aktuell_bearbeiten_GET

--- app/components/aktuell_bearbeiten_GET.php
+++ app/components/aktuell_bearbeiten_GET.php
@@ -8,24 +8,16 @@ $output;
 
 
 
-if ($stmt = $mysqli->prepare('SELECT title, bezeichnung, datum, vorname, nachname from Aktuelles join YogaLehrer on YogaLehrer.Lehrer_ID = .FK_Lehrer where YogaLehrer.Lehrer_ID = ?')) {
+if ($stmt = $mysqli->prepare('SELECT title, bezeichnung, datum, vorname, nachname from Aktuelles join YogaLehrer on YogaLehrer.Lehrer_ID = Aktuelles.FK_Lehrer where YogaLehrer.Lehrer_ID = ?')) {
     $stmt->bind_param('i', $id);
     $stmt->execute();
-    $stmt->bind_result($vname, $nname, $email, $telefonnummer, $passwort, $adresse, $adresszusatz, $plz, $ort, $land, $pbname, $pbpfad, $pbversteckt);
+    $stmt->bind_result($titel,$bezeichnung, $datum, $vorname, $nachname);
     while ($stmt->fetch()) {
-        $output['vorname'] = $vname;
-        $output['nachname'] = $nname;
-        $output['email'] = $email;
+        $output['titel'] = $title;
+        $output['bezeichnung'] = $bezeichnung;
+        $output['datum'] = $datum;
         $output['telefonnummer'] = $telefonnummer;
         $output['passwort'] = $passwort;
-        $output['adresse'] = $adresse;
-        $output['adresszusatz'] = $adresszusatz;
-        $output['plz'] = $plz;
-        $output['ort'] = $ort;
-        $output['land'] = $land;
-        $output['profilbildname'] = $pbname;
-        $output['profilbildpfad'] = $pbpfad;
-        $output['profilbildversteckt'] = $pbversteckt;
     }
     $stmt->close();
 
