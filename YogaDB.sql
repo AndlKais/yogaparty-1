79cf0d6538cb3a645526e1713ed141ec61aa5797
KaiserStefan <31877980+KaiserStefan@users.noreply.github.com>
2018-06-11 18:25:11 +0200

    datenbank update

--- YogaDB.sql
+++ YogaDB.sql
@@ -11,14 +11,14 @@ email varchar(30),
 telefonnummer integer,
 passwort varchar(30),
 adresse varchar(30),
-adresszusatz varchar(20),
+adresszusatz varchar(30),
 plz integer,
 ort varchar(30),
 land varchar(30),
 ist_freigegeben boolean
 );
 
-insert into YogaLehrer (vorname, nachname, email, telefonnummer, passwort, adresse, adresszusatz, plz, ort, land, ist_freigegeben) values ('Jens', 'Oberberger', 'jens.oberberger@gmail.com', 0664653721, 'asdf', 'Rennweg', '89b', 1030, 'Wien', 'AT',true);
+insert into YogaLehrer (Lehrer_ID, vorname, nachname, email, telefonnummer, passwort, adresse, adresszusatz, plz, ort, land, ist_freigegeben) values (1, 'Jens', 'Oberberger', 'Jens.Wappler@trashmail.com', 0664653721, 'Blaumeisenmaschine', 'Baumgasse', 27, 1030, 'Wupperntal', 'AT',true);
 
 create table if not exists Zertifikat (
 Zert_ID integer primary key auto_increment,
@@ -77,7 +77,7 @@ bgcolor varchar(30),
 Foreign Key (FK_Seiten_ID) references Profilseite(Seiten_ID)
 );
 
-insert into PSBlock (blockart, position, FK_seiten_ID) values
+insert into PSBlock (blockart, position, FK_seiten_ID) values 
 ('BTCT', 1, 1),
 ('BBCT', 2, 1),
 ('BBT', 3, 1),
@@ -99,12 +99,13 @@ bName varchar(200),
 bPfad varchar(30),
 titel varchar(30),
 text varchar(500),
+bZahl integer,
 Foreign Key (BBCText_ID) references PSBlock(Block_ID) ON DELETE CASCADE
 );
 
-insert into B_Bild_C_Text (BBCText_ID, bName, bPfad, titel, text) values
-(2, 'https://milltowngalway.com/wp-content/uploads/2018/01/yog.jpg', '','Lorem Ipsum','Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'),
-(4, 'https://image.brigitte.de/10208872/uncropped-0-0/b7b47e9612a86776bfb168a64774ca91/yJ/yoga-frau-natur-t.jpg','','Lorem Ipsum', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.');
+insert into B_Bild_C_Text (BBCText_ID, bName, bPfad, titel, text, bZahl) values 
+(2, 'https://milltowngalway.com/wp-content/uploads/2018/01/yog.jpg', '','Lorem Ipsum','Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 1), 
+(4, 'https://image.brigitte.de/10208872/uncropped-0-0/b7b47e9612a86776bfb168a64774ca91/yJ/yoga-frau-natur-t.jpg','','Lorem Ipsum', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 2);
 
 create table if not exists B_Bild_Text (
 BBText_ID integer primary key,
@@ -113,26 +114,31 @@ bName varchar(200),
 bPfad varchar(30),
 titel varchar(30),
 text varchar(500),
+bZahl integer,
 Foreign Key (BBText_ID) references PSBlock(Block_ID) ON DELETE CASCADE
 );
 
-insert into B_Bild_Text (BBText_ID, bildR, bName, bPfad, titel, text) values (3, true, 'https://bbluntdaily.com/wp-content/uploads/2018/04/pexels-photo-588561.jpg', '', 'Lorem Ispum', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.');
-insert into B_Bild_Text (BBText_ID, bildR, bName, bPfad, titel, text) values (5, false, 'https://bbluntdaily.com/wp-content/uploads/2018/04/pexels-photo-588561.jpg', '', 'Lorem Ispum', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.');
+insert into B_Bild_Text (BBText_ID, bildR, bName, bPfad, titel, text, bZahl) values (3, true, 'https://bbluntdaily.com/wp-content/uploads/2018/04/pexels-photo-588561.jpg', '', 'Lorem Ispum', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',1);
+insert into B_Bild_Text (BBText_ID, bildR, bName, bPfad, titel, text, bZahl) values (5, false, 'https://bbluntdaily.com/wp-content/uploads/2018/04/pexels-photo-588561.jpg', '', 'Lorem Ispum', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',2);
 
 
-create table if not exists aktuelles (
+create table if not exists Aktuelles (
 aktuell_ID integer primary key auto_increment,
-description varchar(200),
+titel varchar(200),
+bezeichnung varchar(200),
 datum date,
 FK_Lehrer integer,
 Foreign Key(FK_Lehrer) references YogaLehrer(Lehrer_ID) ON DELETE CASCADE
 );
 
+insert into Aktuelles(titel, bezeichnung, datum, FK_Lehrer) values ('Das neue Yogastudio neben der HTL Rennweg hat heute eröffnet', 'Dies ist ein Test. Wird gleich wieder entfernt', '2018-06-10',1);
+SELECT titel, bezeichnung, datum, vorname, nachname from Aktuelles join YogaLehrer on YogaLehrer.Lehrer_ID = Aktuelles.FK_Lehrer where YogaLehrer.Lehrer_ID = 1;
 /*create table if not exists news (
 
 );*/
 
 select * from YogaLehrer;
+select * from Profilseite;
 
 SELECT vorname, nachname, email, telefonnummer, passwort, adresse, adresszusatz, plz, ort, land from YogaLehrer where Lehrer_ID=1;
 
@@ -143,7 +149,7 @@ select * from psblock;
 
 select * from (select Block_ID, position, titel, text, null as bPfad, null as bName, null as bildR from B_Titel_C_Text
 	join PSBlock on (BTCText_ID = Block_ID) where FK_Seiten_ID = 1
-union
+union 
 select Block_ID, position, titel, text, bPfad, bName, null  from B_Bild_C_Text
 	join PSBlock on (BBCText_ID = Block_ID) where FK_Seiten_ID = 1
 union
